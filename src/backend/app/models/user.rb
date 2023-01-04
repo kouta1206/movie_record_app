fac0030a5c3fc98b4b9260770094b0e0ffde8725
kouta1206 <ko.bump1206@icloud.com>
2023-01-04 11:17:59 +0900

    userモデルのemailカラムのバリデーションを追記

--- src/backend/app/models/user.rb
+++ src/backend/app/models/user.rb
@@ -1,4 +1,7 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+    before_validation   :downcase_email
     has_secure_password
 
     validates :name, presence: true,
@@ -6,6 +9,10 @@ class User < ApplicationRecord
                                 maximum: 30,
                                 allow_blank: true
                             }
+
+    validates :email, presence: true,
+                            email: { allow_blank: true }
+
     VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
     validates :password, presence: true,
                             length: {
@@ -18,4 +25,20 @@ class User < ApplicationRecord
                                 allow_blank: true
                             },
                             allow_nil: true
+    class << self
+        def find_by_activated(email)
+            find_by(email: email, activated: true)
+        end
+    end
+
+    def email_activated?
+        users = User.where.not(id: id)
+        users.find_by_activated(email).present?
+    end
+
+    private
+
+    def downcase_email
+        self.email.downcase! if email
+    end
 end
