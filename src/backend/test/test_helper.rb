9abc065353cf09149e249b3e9bd0f349e7f51c96
kouta1206 <ko.bump1206@icloud.com>
2023-01-04 13:30:20 +0900

    userモデルのバリデーションテスト設定を追記

--- src/backend/test/test_helper.rb
+++ src/backend/test/test_helper.rb
@@ -2,7 +2,14 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# gem minitest-reporters setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  parallelize_setup do |worker|
+      load "#{Rails.root}/db/seeds.rb"
+  end
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
