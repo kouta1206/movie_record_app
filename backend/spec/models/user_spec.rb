7792746f57ed667fb3957739a61013216cc02b85
kouta1206 <ko.bump1206@icloud.com>
2023-03-02 22:27:53 +0900

    テスト修正

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+ describe '登録処理テスト' do
+  context '異常系' do
+    it '名前が必須であること' do
+      user = User.new(email: "test@example.com", password: "password")
+      user.save
+      expect(user.errors[:name]).to include(I18n.t('errors.messages.blank'))
+      end
+      it '31文字以上はバリデーションの発生すること' do
+        max = 30
+        name = "a" * (max + 1)
+        user = User.new(email: "test@example.com", password: "password", name: name)
+        user.save
+
+        expect(user.errors[:name]).to include(I18n.t('errors.messages.too_long'))
+        
+      end
+    end
+  end
 end
